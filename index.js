2628f6407cb7a53cfa38796521b1e04eaf94312f
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-11-04 04:05:29 +0300

    so empty commit

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict'; 
+const express = require('express'); 
+const bodyParser = require('body-parser'); 
+const app = express(); 
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+const server = app.listen(3000, () => { 
+	console.log('Express server   listening on port %d in %s mode', 
+		server.address().port,   
+		app.settings.env);
+});
+
+
+app.post('/', (req, res) => { 
+  let text = req.body.text; 
+  // implement your bot here ... 
+
+	if(! /^\d+$/.test(q.text)) { // not a digit 
+		res.send('U R DOIN IT WRONG. Enter a status code like 200!');   return; 
+	}
+
+});
